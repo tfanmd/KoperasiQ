a429cf8a58ed85d6692d022fe53f843108084b91
Ahmad Taufan Hidayat <ahmdtaufan122004@gmail.com>
2026-06-05 19:42:49 +0700

    menambahkan enpoint insert member

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from database import engine
 import models
+import schemas
 
 models.Base.metadata.create_all(bind=engine)  # Membuat tabel di database jika belum ada
 # Inisiasi aplikasi FastAPI
@@ -17,3 +18,19 @@ def read_root():
         "status": "Sukses", 
         "pesan": "Server FastAPI Koperasi Sembako sudah menyala! 🚀"
     }
+
+# enpoint member
+
+@app.post("/api/members/", response_model=schemas.MemberResponse)
+def create_member(member: schemas.MemberCreate, db: Session = Depends(get_db)):
+    # endpoint untuk membuat member baru
+    db_member = db.query(models.Member).filter(models.Member.identity_no == member.identity_no).first()
+    if db_member:
+        raise HTTPException(status_code=400, detail="Member dengan nomor identitas ini sudah ada")
+    # simpan data member baru ke database
+    new_member = models.Member(name=member.name, identity_no=member.identity_no)
+    # proses eksekusi penyimpanan data member baru ke database
+    db.add(new_member)
+    db.commit()
+    db.refresh(new_member)
+    return new_member
